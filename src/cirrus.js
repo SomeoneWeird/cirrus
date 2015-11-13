29971a9127be01bbc2ee3a2596a3cbeeadfceb07
Adam Brady <adam@boxxen.org>
2015-11-13 18:11:40 +0800

    add more commands

--- src/cirrus.js
+++ src/cirrus.js
@@ -1,26 +1,38 @@
 #!/usr/bin/env node
 
+import path   from "path";
+import fs     from "fs";
 import AWS    from "aws-sdk";
 import yargs  from "yargs";
 import Table  from "cli-table";
 import moment from "moment";
+import open   from "open";
 
 const argv = yargs
               .usage('Usage: $0 <command>')
               .command('list', 'List all non-deleted stacks')
               .command('info', 'Returns information about the stack')
+              .command('account', 'Returns information about your AWS account')
+              .command('estimate', 'Returns monthly cost estimate of stack')
               .describe('region', 'Set region')
               .describe('showdeleted', 'Show deleted stacks')
+              .describe('file', 'Template file')
+              .describe('parameters', 'Template file parameters')
               .default('region', 'ap-southeast-1')
               .default('showdeleted', false)
+              .alias('file', 'f')
+              .alias('parameters', 'params')
+              .alias('parameters', 'p')
               .demand(1, '')
               .argv;
 
 let cmd = argv._[0];
 
 let commands = {
-  list: listStacks,
-  info: stackInfo
+  list:     listStacks,
+  info:     stackInfo,
+  account:  accountInfo,
+  estimate: estimateCost
 }
 
 if(!~Object.keys(commands).indexOf(cmd)) {
@@ -52,6 +64,35 @@ function fetchStacks(callback) {
     });
   }
   fetchStack();
+}
+
+function getTemplate() {
+
+  if(!argv.file) {
+    console.error("Please pass '--file <filename>'");
+    process.exit(1);
+  }
+
+  if(!argv.parameters) {
+    console.error("Please pass '--parameters <parameters file>'");
+    process.exit(1);
+  }
+
+  let file, params;
+
+  try {
+    file   = JSON.parse(fs.readFileSync(path.resolve(__dirname, argv.file)).toString());
+    params = JSON.parse(fs.readFileSync(path.resolve(__dirname, argv.parameters)).toString());
+  } catch(e) {
+    console.error(`There was an error loading your template/params file: ${e}`);
+    process.exit(1);
+  }
+
+  return {
+    file,
+    params
+  };
+
 }
 
 function listStacks() {
@@ -154,6 +195,49 @@ function stackInfo() {
       console.log(table.toString());
 
     });
+
+  });
+
+}
+
+function accountInfo() {
+
+  cloudformation.describeAccountLimits({}, function(err, response) {
+
+    if(err) {
+      throw new Error(err);
+    }
+
+    fetchStacks(function(err, stacks) {
+
+      if(err) {
+        throw new Error(err);
+      }
+
+      stacks = stacks.filter(stack => stack.StackStatus !== 'DELETE_COMPLETE');
+
+      console.log(`You have currently used ${stacks.length} out of ${response.AccountLimits[0].Value} stacks`);
+
+    });
+
+  });
+
+}
+
+function estimateCost() {
+
+  const template = getTemplate();
+
+  cloudformation.estimateTemplateCost({
+    TemplateBody: JSON.stringify(template.file),
+    Parameters:   template.params
+  }, function(err, response) {
+
+    if(err) {
+      throw new Error(err);
+    }
+
+    open(response.Url);
 
   });
 
